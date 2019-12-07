86b16b94bc7063a15a6a65c66efdf7e12e3fffe2
Angela <angela.kramer@posteo.de>
2019-12-07 22:24:55 +0100

    * replace vector by linked list * solved day 3 part 2

--- 03/src/main.cpp
+++ 03/src/main.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <iostream>
 #include <sstream>
 #include <stdexcept>
@@ -10,144 +11,215 @@ struct Point {
   int y;
 };
 
+int Distance(Point a, Point b) {
+  return std::abs(a.x - b.x) + std::abs(a.y - b.y);
+}
+
 enum class Direction { UNKNOWN, VERTICAL, HORIZONTAL };
 
-struct WireSegment {
-  Point start = {0, 0};
-  Point end = {0, 0};
-  int length;
-  Direction direction = Direction::UNKNOWN;
+struct Node {
+  Point point = {0, 0};
+  Node *next_node = NULL;
+  Direction direction_from_prev_node = Direction::UNKNOWN;
+  int distance_from_prev_node = 0;
+  int distance_from_origin = 0;
+
+  bool DirectionIs(Direction direction) {
+    return this->next_node->direction_from_prev_node == direction;
+  }
 };
 
-std::istream &operator>>(std::istream &is, WireSegment &output) {
-  std::string value_string;
-  std::getline(is, value_string, ',');
-  if (value_string.size() > 0) {
-    auto direction = value_string[0];
-    switch (direction) {
-    case 'R':
-      output.direction = Direction::HORIZONTAL;
-      output.length = std::stoi(value_string.substr(1));
-      break;
-    case 'L':
-      output.direction = Direction::HORIZONTAL;
-      output.length = -std::stoi(value_string.substr(1));
-      break;
-    case 'U':
-      output.direction = Direction::VERTICAL;
-      output.length = std::stoi(value_string.substr(1));
-      break;
-    case 'D':
-      output.direction = Direction::VERTICAL;
-      output.length = -std::stoi(value_string.substr(1));
-      break;
-    default:
-      throw std::runtime_error("unknown direction " + direction);
-    }
+class Intersection {
+public:
+  Intersection(Node *horizontal_segment, Node *vertical_segment) {
+
+    point_.x = vertical_segment->point.x;
+    point_.y = horizontal_segment->point.y;
+
+    combined_steps_from_origin_ = horizontal_segment->distance_from_origin +
+                                  Distance(point_, horizontal_segment->point) +
+                                  vertical_segment->distance_from_origin +
+                                  Distance(point_, vertical_segment->point);
   }
-  return is;
+
+  virtual ~Intersection(){};
+
+  Point GetPoint() { return point_; }
+
+  int CombinedStepsFromOrigin() { return combined_steps_from_origin_; }
+
+private:
+  Point point_;
+  int combined_steps_from_origin_;
 };
 
-std::vector<WireSegment> GenerateWire(std::string line) {
-  std::stringstream ss(line);
-  std::vector<WireSegment> wire{std::istream_iterator<WireSegment>(ss),
-                                std::istream_iterator<WireSegment>()};
-  for (int i = 0; i < wire.size(); ++i) {
-    if (i > 0) {
-      wire[i].start = wire[i - 1].end;
-    }
+Node *Wire(std::string line) {
 
-    switch (wire[i].direction) {
-    case Direction::HORIZONTAL:
-      wire[i].end.x = wire[i].start.x + wire[i].length;
-      wire[i].end.y = wire[i].start.y;
-      break;
-    case Direction::VERTICAL:
-      wire[i].end.x = wire[i].start.x;
-      wire[i].end.y = wire[i].start.y + wire[i].length;
-      break;
-    default:
-      throw std::runtime_error("direction not set");
-    }
-  }
+  Node *origin = new Node{};
 
-  return wire;
-}
+  // extract direction and distance
+  {
+    Node *current_node = origin;
 
-bool Intersects(WireSegment horizontal_segment, WireSegment vertical_segment) {
-  return (vertical_segment.start.y < horizontal_segment.start.y &&
-          horizontal_segment.start.y < vertical_segment.end.y &&
-          horizontal_segment.start.x < vertical_segment.start.x &&
-          vertical_segment.start.x < horizontal_segment.end.x) ||
-         (vertical_segment.start.y > horizontal_segment.start.y &&
-          horizontal_segment.start.y > vertical_segment.end.y &&
-          horizontal_segment.start.x > vertical_segment.start.x &&
-          vertical_segment.start.x > horizontal_segment.end.x);
-}
+    std::stringstream ss(line);
+    std::string value_string;
+    while (std::getline(ss, value_string, ',')) {
+      if (value_string.size() > 0) {
 
-std::vector<Point> FindIntersections(std::vector<WireSegment> wire1,
-                                     std::vector<WireSegment> wire2) {
-  std::vector<Point> intersections;
+        current_node->next_node = new Node{};
+        current_node = current_node->next_node;
 
-  for (const auto &segment1 : wire1) {
-    std::cout << "wire1 " << segment1.start.x << " / " << segment1.start.y
-              << " <--> " << segment1.end.x << " / " << segment1.end.y
-              << std::endl;
-  }
-  for (const auto &segment2 : wire2) {
-    std::cout << "wire2 " << segment2.start.x << " / " << segment2.start.y
-              << " <--> " << segment2.end.x << " / " << segment2.end.y
-              << std::endl;
-  }
-
-  for (const auto &segment1 : wire1) {
-    for (const auto &segment2 : wire2) {
-      if (segment1.direction == Direction::HORIZONTAL &&
-          segment2.direction == Direction::VERTICAL) {
-        if (Intersects(segment1, segment2)) {
-          intersections.emplace_back(Point{segment2.start.x, segment1.start.y});
-          std::cout << "INTERSECT" << std::endl;
-          std::cout << "wire1 " << segment1.start.x << " / " << segment1.start.y
-                    << " <--> " << segment1.end.x << " / " << segment1.end.y
-                    << std::endl;
-          std::cout << "wire2 " << segment2.start.x << " / " << segment2.start.y
-                    << " <--> " << segment2.end.x << " / " << segment2.end.y
-                    << std::endl;
-        }
-
-      } else if (segment1.direction == Direction::VERTICAL &&
-                 segment2.direction == Direction::HORIZONTAL) {
-        if (Intersects(segment2, segment1)) {
-          intersections.emplace_back(Point{segment1.start.x, segment2.start.y});
-          std::cout << "INTERSECT" << std::endl;
-          std::cout << "wire1 " << segment1.start.x << " / " << segment1.start.y
-                    << " <--> " << segment1.end.x << " / " << segment1.end.y
-                    << std::endl;
-          std::cout << "wire2 " << segment2.start.x << " / " << segment2.start.y
-                    << " <--> " << segment2.end.x << " / " << segment2.end.y
-                    << std::endl;
+        auto direction = value_string[0];
+        switch (direction) {
+        case 'R':
+          current_node->direction_from_prev_node = Direction::HORIZONTAL;
+          current_node->distance_from_prev_node =
+              std::stoi(value_string.substr(1));
+          break;
+        case 'L':
+          current_node->direction_from_prev_node = Direction::HORIZONTAL;
+          current_node->distance_from_prev_node =
+              -std::stoi(value_string.substr(1));
+          break;
+        case 'U':
+          current_node->direction_from_prev_node = Direction::VERTICAL;
+          current_node->distance_from_prev_node =
+              std::stoi(value_string.substr(1));
+          break;
+        case 'D':
+          current_node->direction_from_prev_node = Direction::VERTICAL;
+          current_node->distance_from_prev_node =
+              -std::stoi(value_string.substr(1));
+          break;
+        default:
+          throw std::runtime_error("unknown direction " + direction);
         }
       }
     }
-    std::cout << std::endl;
   }
 
-  for (auto intersection : intersections) {
-    std::cout << "intersection: " << intersection.x << " / " << intersection.y
-              << std::endl;
+  {
+    Node *current_node = origin;
+    while (current_node->next_node) {
+
+      // calculate coordinates
+      switch (current_node->next_node->direction_from_prev_node) {
+      case Direction::HORIZONTAL:
+        current_node->next_node->point.x =
+            current_node->point.x +
+            current_node->next_node->distance_from_prev_node;
+        current_node->next_node->point.y = current_node->point.y;
+        break;
+      case Direction::VERTICAL:
+        current_node->next_node->point.x = current_node->point.x;
+        current_node->next_node->point.y =
+            current_node->point.y +
+            current_node->next_node->distance_from_prev_node;
+        break;
+      default:
+        throw std::runtime_error("direction not set");
+      }
+
+      // calculate distance from origin
+      current_node->next_node->distance_from_origin =
+          current_node->distance_from_origin +
+          std::abs(current_node->next_node->distance_from_prev_node);
+
+      current_node = current_node->next_node;
+    }
+  }
+
+  return origin;
+}
+
+void PrintWireSegment(Node *origin) {
+  std::cout << origin->point.x << " / " << origin->point.y << " <--> "
+            << origin->next_node->point.x << " / " << origin->next_node->point.y
+            << std::endl;
+}
+
+void PrintWire(Node *origin) {
+  Node *current_node = origin;
+  while (current_node->next_node) {
+    std::cout << "wire1 " << current_node->point.x << " / "
+              << current_node->point.y << std::endl;
+    current_node = current_node->next_node;
+  }
+}
+
+Node *GetNodeByDirection(Direction direction, Node *node1, Node *node2) {
+  if (node1->DirectionIs(direction))
+    return node1;
+  if (node2->DirectionIs(direction))
+    return node2;
+  return NULL;
+}
+
+bool IsInbetween(int a, int b, int c) {
+  return (a < b && b < c) || (a > b && b > c);
+}
+
+bool XCoordinatesIntersect(Node *segment, Point point) {
+  return IsInbetween(segment->point.x, point.x, segment->next_node->point.x);
+}
+
+bool YCoordinatesIntersect(Node *segment, Point point) {
+  return IsInbetween(segment->point.y, point.y, segment->next_node->point.y);
+}
+
+bool Intersects(Node *horizontal_segment, Node *vertical_segment) {
+  return (XCoordinatesIntersect(horizontal_segment, vertical_segment->point) &&
+          YCoordinatesIntersect(vertical_segment, horizontal_segment->point));
+}
+
+std::vector<Intersection> FindIntersections(Node *wire1_origin,
+                                            Node *wire2_origin) {
+  std::vector<Intersection> intersections;
+
+  Node *current_node1 = wire1_origin;
+  while (current_node1->next_node) {
+
+    Node *current_node2 = wire2_origin;
+    while (current_node2->next_node) {
+
+      auto horizontal_segment = 
+          GetNodeByDirection(Direction::HORIZONTAL, current_node1, current_node2);
+      auto vertical_segment =
+          GetNodeByDirection(Direction::VERTICAL, current_node1, current_node2);
+
+      if (horizontal_segment && vertical_segment &&
+          Intersects(horizontal_segment, vertical_segment)) {
+        intersections.emplace_back(horizontal_segment, vertical_segment);
+      }
+
+      current_node2 = current_node2->next_node;
+    }
+
+    current_node1 = current_node1->next_node;
   }
 
   return intersections;
 }
 
-int ClosestIntersection(std::vector<Point> intersections) {
+int ClosestIntersection(std::vector<Intersection> intersections) {
+  Point origin{0, 0};
   std::sort(std::begin(intersections), std::end(intersections),
-            [](Point a, Point b) {
-              return (std::abs(a.x) + std::abs(a.y)) <
-                     (std::abs(b.x) + std::abs(b.y));
+            [&](Intersection a, Intersection b) {
+              return Distance(a.GetPoint(), origin) <
+                     Distance(b.GetPoint(), origin);
             });
 
-  return std::abs(intersections[0].x) + std::abs(intersections[0].y);
+  return std::abs(intersections.front().GetPoint().x) +
+         std::abs(intersections.front().GetPoint().y);
+}
+
+int IntersectionWithMinimalDelay(std::vector<Intersection> intersections) {
+  std::sort(std::begin(intersections), std::end(intersections),
+            [&](Intersection a, Intersection b) {
+              return a.CombinedStepsFromOrigin() < b.CombinedStepsFromOrigin();
+            });
+
+  return intersections.front().CombinedStepsFromOrigin();
 }
 
 int main() {
@@ -155,12 +227,16 @@ int main() {
   std::ifstream input_file("../input/input.txt");
 
   std::getline(input_file, line);
-  auto wire1 = GenerateWire(line);
+  auto wire1 = Wire(line);
 
   std::getline(input_file, line);
-  auto wire2 = GenerateWire(line);
+  auto wire2 = Wire(line);
 
   auto intersections = FindIntersections(wire1, wire2);
+
+  if (intersections.size() == 0) {
+    throw std::runtime_error("no intersections");
+  }
 
   // part 1
   {
@@ -169,7 +245,8 @@ int main() {
 
   // part 2
   {
-    // std::cout << "part 2: " << IntersectionWithMinimalDelay(intersections) << std::endl;
+    std::cout << "part 2: " << IntersectionWithMinimalDelay(intersections)
+              << std::endl;
   }
 
   return EXIT_SUCCESS;
