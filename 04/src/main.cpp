022b4bbe630814a5a3a97d5a4a5b6a9b529c7a06
Angela <angela.kramer@posteo.de>
2019-12-09 21:10:06 +0100

    some minor improvements

--- 04/src/main.cpp
+++ 04/src/main.cpp
@@ -1,90 +1,112 @@
 #include <cmath>
+#include <functional>
 #include <iostream>
 #include <vector>
 
 #include "../../common.h"
 
-std::vector<int> passwords{};
-
-std::vector<int> input_min_digits{};
-
-int input_min{231832};
-int input_max{767346};
-
-bool HasConsicutiveDigits(int number) {
-  while (number) {
-    auto last_two_digits = number % 100;
-    if (last_two_digits % 11 == 0) {
-      return true;
-    }
-    number /= 10;
+class PasswordGenerator {
+public:
+  PasswordGenerator(const int input_min, const int input_max,
+                    std::function<bool(int)> condition)
+      : input_min_(input_min), input_max_(input_max), condition_(condition),
+        input_min_digits_(FirstValidNumber(Digits(input_min))) {
+    ;
   }
 
-  return false;
-}
+  virtual ~PasswordGenerator() {}
 
-void Password(int previous_digit, int power, int intermediate_sum) {
-  if (power >= 0) {
-    auto i_init = previous_digit;
-
-    if (passwords.size() == 0) {
-      i_init = input_min_digits[power];
-    }
-
-    for (int i = i_init; i <= 9; ++i) {
-      Password(i, power - 1, intermediate_sum + i * pow(10, power));
-    }
-  } else {
-    if (intermediate_sum > input_max) {
-      return;
-    }
-
-    if (HasConsicutiveDigits(intermediate_sum)) {
-      passwords.push_back(intermediate_sum);
-    }
-  }
-}
-
-std::vector<int> Digits(int number) {
-  std::vector<int> digits;
-
-  while (number) {
-    auto digit = number % 10;
-    number /= 10;
-    digits.push_back(digit);
+  std::vector<int> Passwords() {
+    Password(input_min_digits_.back(), input_min_digits_.size() - 1, 0);
+    return passwords_;
   }
 
-  return digits;
-}
+private:
+  std::vector<int> Digits(int number) const {
+    std::vector<int> digits;
 
-std::vector<int> FirstValidNumber(std::vector<int> digits) {
-  int max_digit = digits.back();
-  bool overwrite = false;
+    do {
+      auto digit = number % 10;
+      digits.push_back(digit);
+    } while (number /= 10);
 
-  for (int i = digits.size() - 1; i >= 0; --i) {
-    if (digits[i] < max_digit || overwrite) {
-      digits[i] = max_digit;
-      overwrite = true;
-    } else if (digits[i] > max_digit) {
-      max_digit = digits[i];
+    return digits;
+  }
+
+  std::vector<int> FirstValidNumber(std::vector<int> digits) const {
+    int max_digit = digits.back();
+    bool overwrite = false;
+
+    for (auto digit = std::rbegin(digits); digit != std::rend(digits); digit++) {
+      if (*digit < max_digit || overwrite) {
+        *digit = max_digit;
+        overwrite = true;
+      } else if (*digit > max_digit) {
+        max_digit = *digit;
+      }
+    }
+
+    return digits;
+  }
+
+  void Password(const int previous_digit, const int power,
+                const int intermediate_sum) {
+    if (power >= 0) {
+      auto i_init = previous_digit;
+
+      if (passwords_.size() == 0) {
+        i_init = input_min_digits_[power];
+      }
+
+      for (size_t i = i_init; i <= 9; ++i) {
+        Password(i, power - 1, intermediate_sum + i * pow(10, power));
+      }
+    } else {
+      if (intermediate_sum > input_max_) {
+        return;
+      }
+
+      if (condition_(intermediate_sum)) {
+        passwords_.push_back(intermediate_sum);
+      }
     }
   }
-  return digits;
-}
+
+  int input_min_;
+  int input_max_;
+
+  std::function<bool(int)> condition_;
+  std::vector<int> input_min_digits_;
+  std::vector<int> passwords_;
+};
 
 int main() {
-  input_min_digits = FirstValidNumber(Digits(input_min));
-  Password(input_min_digits.back(), input_min_digits.size() - 1, 0);
-
-  for (const auto &password : passwords) {
-    std::cout << password << std::endl;
-  }
+  int input_min{231832};
+  int input_max{767346};
 
   // part 1
-  { std::cout << "part 1: " << passwords.size() << std::endl; }
+  {
+    auto has_double_digits = [](int number) -> bool {
+      do {
+        auto last_two_digits = number % 100;
+        if (last_two_digits % 11 == 0) {
+          return true;
+        }
+      } while (number /= 10);
+
+      return false;
+    };
+
+    PasswordGenerator pw_gen{input_min, input_max, has_double_digits};
+    auto passwords = pw_gen.Passwords();
+
+    std::cout << "part 1: " << passwords.size() << std::endl;
+  }
 
   // part 2
-  // { std::cout << "part 2: " << ??? << std::endl; }
+  {
+    // std::cout << "part 2: " << passwords.size() << std::endl;
+  }
 
   return EXIT_SUCCESS;
 }
